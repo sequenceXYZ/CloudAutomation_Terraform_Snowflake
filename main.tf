4cbfca2ff9e9a5a4a8a894bf55a9519ab9264ea8
sequenceXYZ <122634843+sequenceXYZ@users.noreply.github.com>
2024-02-09 15:32:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# create rsource warehouse
 resource "snowflake_warehouse" "example_warehouse" {
   name                = var.warehouse_name
   warehouse_size      = var.warehouse_size
@@ -6,15 +7,18 @@ resource "snowflake_warehouse" "example_warehouse" {
   initially_suspended = var.initially_suspended
 }
 
+# create resource database
 resource "snowflake_database" "example_database" {
   name = var.database_name
 }
 
+# create resource schema
 resource "snowflake_schema" "example_schema" {
   name     = var.schema_name
   database = snowflake_database.example_database.name
 }
 
+# create resource table
 resource "snowflake_table" "example_table" {
   database = snowflake_database.example_database.name
   schema   = snowflake_schema.example_schema.name
@@ -37,16 +41,19 @@ resource "snowflake_table" "example_table" {
   }
 }
 
+# create resource role
 resource "snowflake_role" "example_role" {
   name = var.role_name
 }
 
+# grant database access to new user
 resource "snowflake_database_grant" "example_database_grant" {
   database_name = snowflake_database.example_database.name
   privilege     = "USAGE"
   roles         = [var.user_role]
 }
 
+# grant schema access to new user
 resource "snowflake_schema_grant" "example_schema_grant" {
   schema_name   = snowflake_schema.example_schema.name
   database_name = snowflake_database.example_database.name
@@ -54,12 +61,14 @@ resource "snowflake_schema_grant" "example_schema_grant" {
   roles         = [var.user_role]
 }
 
+# grant warehouse access to new user
 resource "snowflake_warehouse_grant" "example_warehouse_grant" {
   warehouse_name = snowflake_warehouse.example_warehouse.name
   privilege      = "MODIFY"
   roles          = [var.user_role]
 }
 
+# grant table access to new user
 resource "snowflake_table_grant" "example_table_grant" {
   table_name    = snowflake_table.example_table.name
   schema_name   = snowflake_schema.example_schema.name
@@ -68,12 +77,13 @@ resource "snowflake_table_grant" "example_table_grant" {
   roles         = [var.user_role]
 }
 
-
+# create resource new user
 resource "snowflake_user" "new_user" {
   name     = var.user_name
   password = var.user_password
 }
 
+# grant role to new user
 resource "snowflake_role_grants" "user_role_grants" {
   role_name = var.user_role
 
