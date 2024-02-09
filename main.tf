13b0900a20b86d06c2373f20c7342b4a8bf6fd88
sequenceXYZ <122634843+sequenceXYZ@users.noreply.github.com>
2024-02-09 15:13:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,3 +67,48 @@ resource "snowflake_table_grant" "example_table_grant" {
   privilege     = "SELECT"
   roles         = [snowflake_role.example_role.name]
 }
+
+
+
+
+
+resource "snowflake_user" "new_user" {
+  name     = var.user_name
+  password = var.user_password
+}
+
+resource "snowflake_role_grants" "user_role_grants" {
+  role_name = var.user_role
+
+  users = [
+    snowflake_user.new_user.name,
+  ]
+}
+
+resource "snowflake_warehouse_grant" "example_warehouse_grant" {
+  warehouse_name = var.warehouse_name
+  privilege      = "USAGE"
+  roles          = [var.user_role]
+}
+
+resource "snowflake_database_grant" "example_database_grant" {
+  database_name = var.database_name
+  privilege     = "USAGE"
+  roles         = [var.user_role]
+}
+
+resource "snowflake_schema_grant" "example_schema_grant" {
+  schema_name   = var.schema_name
+  database_name = var.database_name
+  privilege     = "USAGE"
+  roles         = [var.user_role]
+}
+
+resource "snowflake_table_grant" "example_table_grant" {
+  table_name    = var.table_name
+  schema_name   = var.schema_name
+  database_name = var.database_name
+  privilege     = "SELECT"
+  roles         = [var.user_role]
+}
+
